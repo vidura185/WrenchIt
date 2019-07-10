f3a3361030b281ca4d9f687907c1370c5a647c41
vidura185 <huwh3c@gmail.com>
2019-07-10 15:33:53 +0530

    reorganized-file-structure
    File Structure reorganized

---
+++ Assets/Scripts/Player/Spanner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpannerController : MonoBehaviour
+{
+    public Transform pivotOne;
+    public Transform pivotTwo;
+    private Collider pivotOneCollider;
+    private Collider pivotTwoCollider;
+
+
+    public Transform attachPivot;
+
+    public float rotateSpeed;
+
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        Rotate();
+    }
+
+    public void Rotate()
+    {
+        if (transform.parent != attachPivot)
+            transform.parent = attachPivot;
+
+        transform.Rotate(attachPivot.transform.position, rotateSpeed * Time.deltaTime);
+    }
+
+    public void OnAttach(Transform newPivot)
+    {
+        attachPivot = newPivot;
+    }
+
+}
+ 
+public class SpannerEnd
+{
+
+}
